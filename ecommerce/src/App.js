914ef1bd00597e910ac7751175e950c547bccb6a
Cdwright93 <80175712+whatupbrownbear@users.noreply.github.com>
2021-06-17 14:12:13 -0400

    added user data to front end

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -5,22 +5,42 @@ import React, { Component } from 'react';
 const api = axios.create({
   baseURL:'https://localhost:44394/api/products/product'
 })
+const api2 = axios.create({
+  baseURL:'https://localhost:44394/api/user/user'
+})
+const api3 = axios.create({
+  baseURL:'https://localhost:44394/api/authentication'
+})
+const token = 
+
 class App extends React.Component {
   constructor(){
     super();
     this.state = {
       Products: [],
+      CurrentUser: [],
     }
   }
   componentDidMount(){
     this.getProducts()
+    this.getUser(token)
   }
   getProducts = async () => {
     let data = await api.get('/').then(({ data }) => data)
     this.setState({ Products : data })
     console.log(this.state.Products)
   }
+  getUser = async (token) => {
+    let data = await api2.get('/', {headers:{"Authorization" : `Bearer ${token}`}}).then(({ data }) => data)
+    this.setState({ CurrentUser : data })
+    console.log(this.state.CurrentUser)
+  }
 render() {
+  if (!this.state.Currentuser){
+    return(
+      <h3>hello not signed in person</h3>
+    )
+  }
   return (
     <div className="App">
       <header className="App-header">
