9c1f0ab640da54514f5a3fdd97b8fa8b1fc275e2
DAVID paredes <augustspies22@gmail.com>
2021-06-21 08:18:38 -0700

    added styling to list and added html to list

--- ecommerce/src/Components/ProductsList.jsx
+++ ecommerce/src/Components/ProductsList.jsx
@@ -12,13 +12,38 @@ const ProductList = ({products, handleProductSelect, CurrentProduct, reviews}) =
         }
         return (
                 <body>
-                    <section className= 'product'>
-                    <header className= 'productheader'>{CurrentProduct.name}</header>
-                    <p className='itemdesc'>{CurrentProduct.description}</p>
-                    <h5 className = 'price'>${CurrentProduct.price}</h5>
-                    <button>Add To Cart</button><button>See Reviews</button>
-                    </section>
+                        
+                    <div class="container">
+		<div class="card">
+			<div class="container-fliud">
+				<div class="wrapper row">
+			
+					<div class="details col-md-6">
+						<h3 class="product-title">{CurrentProduct.name}</h3>
+						<p class="product-description">{CurrentProduct.description}</p>
+						<h4 class="price">current price: <span>${CurrentProduct.price}</span></h4>
+						<h5 class="sizes">sizes:
+							<span class="size" data-toggle="tooltip" title="small">s</span>
+							<span class="size" data-toggle="tooltip" title="medium">m</span>
+							<span class="size" data-toggle="tooltip" title="large">l</span>
+							<span class="size" data-toggle="tooltip" title="xtra large">xl</span>
+						</h5>
+						<h5 class="colors">colors:
+							<span class="color orange not-available" data-toggle="tooltip" title="Not In store"></span>
+							<span class="color green"></span>
+							<span class="color blue"></span>
+						</h5>
+						<div class="action">
+							<button class="add-to-cart btn btn-default" type="button">add to cart</button>
+                            <button class="add-to-cart btn btn-default" type="button">Reviews</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
                 </body>
             )
 }
 export default ProductList;
+
